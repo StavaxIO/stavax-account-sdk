ca858165be14be6579766a72e3e4028a1c279510
Dulig <dulig@xantus.network>
2024-09-17 10:49:05 +0700

    chore: add js docs for config interface

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,18 @@
 import type {Config} from "@wagmi/core";
 
+/**
+ * Configuration options for StavaxAccount.
+ *
+ * @interface StavaxAccountConfig
+ * @property {string} projectID - Stavax Account ProjectID. This is required.
+ * @property {Config} wagmiConfig - Configuration object for Wagmi. This is required.
+ * @property {string} [apiURL] - Optional. URL to override the default API URL.
+ * @property {string} [tgBotWebAppURL] - Optional. URL to override the default Telegram bot web app URL.
+ * @property {boolean} [disableAutoOpenTgBot=false] - Optional. Disables automatic opening of the Telegram bot. Default is `false`.
+ * @property {boolean} [openTgBotOnDesktop=false] - Optional. Whether to open the Telegram bot on desktop. Default is `false`.
+ * @property {number} [requestTimeout=60000] - Optional. Timeout for requests in milliseconds. Default is 60,000 ms (60 seconds).
+ * @property {boolean} [disableSmartSessionFailSafe=false] - Optional. Disables the smart session fail-safe logic. By default, the SDK will fall back to the Wagmi function if the Stavax API responds with an unsuccessful status. Default is `false`.
+ */
 export interface StavaxAccountConfig {
     projectID: string
     wagmiConfig: Config
